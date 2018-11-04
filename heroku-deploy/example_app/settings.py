7ad126753755e1bb2d1c20b7b9344c95b70caf35
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2018-11-04 11:37:40 +0530

    Update heroku-deploy/example_app/settings.py

--- heroku-deploy/example_app/settings.py
+++ heroku-deploy/example_app/settings.py
@@ -1,5 +1,6 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
