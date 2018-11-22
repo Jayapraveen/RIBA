98cc6d9a999d07f05b36f1e3b52d2d0621c56d10
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2018-11-22 13:17:36 +0530

    Update drop_schema.sql

--- drop_schema.sql
+++ drop_schema.sql
@@ -1,1 +1,10 @@
-DROP SCHEMA public CASCADE;
+DO $$ DECLARE
+    tabname RECORD;
+BEGIN
+    FOR tabname IN (SELECT tablename 
+                    FROM pg_tables 
+                    WHERE schemaname = current_schema()) 
+LOOP
+    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(tabname.tablename) || ' CASCADE';
+END LOOP;
+END $$;
