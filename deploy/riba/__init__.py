cf1d78bc35a4b376bf380b96afd977083decf365
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-01-24 20:35:22 +0530

    training addition check

---
+++ deploy/riba/__init__.py
@@ -0,0 +1,6 @@
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot import ChatBot
+
+chatbot = ChatBot('RIBA')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train('chatterbot.corpus.english')
