5419b09173ea7f24817ffe5f1bda95c23d24b898
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-01-31 20:03:39 +0530

    Update settings.py

--- deploy/riba/settings.py
+++ deploy/riba/settings.py
@@ -35,8 +35,6 @@ INSTALLED_APPS = (
 # ChatterBot settings
 
 CHATTERBOT = {
-    'name': 'RIBA',
-	'django_app_name': 'django_chatterbot',
     'logic_adapters' : [
         'chatterbot.logic.MathematicalEvaluation',
         'chatterbot.logic.BestMatch',
