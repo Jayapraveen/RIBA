62d7eed9488165d0e8173abbf9b0713f5ce42e4a
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-01-31 21:04:45 +0530

    Update train_riba.py

--- deploy/train_riba.py
+++ deploy/train_riba.py
@@ -6,6 +6,16 @@ wd = os.getcwd()
 
 chatbot = ChatBot('RIBA')
 trainer = ChatterBotCorpusTrainer(chatbot)
+
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+os.system("wget http://riba-support.000webhostapp.com/RIBA/training-data/rec.yml")
+trainer.train("chatterbot.corpus.english",
+wd + "/rec.yml"
+)
+
+else: #on Travis
 trainer.train("chatterbot.corpus.english",
 wd + "/rec/rec.yml"
 )
