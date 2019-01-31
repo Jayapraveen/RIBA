b1910393d0fc2c5ab7b074242a41c3f56bab6d03
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-01-31 20:35:56 +0530

    Update train_riba.py

--- deploy/train_riba.py
+++ deploy/train_riba.py
@@ -1,8 +1,11 @@
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
+import os
+print os.getcwd()
+wd = os.getcwd()
 
 chatbot = ChatBot('RIBA')
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english",
-"./rec/rec.yml"
+wd + "/rec/rec.yml"
 )
