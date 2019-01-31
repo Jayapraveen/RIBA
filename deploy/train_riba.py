dc1b93a7b66b00487be48a9566d7bf52b907c6e0
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-01-31 20:09:38 +0530

    add rec training files

--- deploy/train_riba.py
+++ deploy/train_riba.py
@@ -3,4 +3,6 @@ from chatterbot import ChatBot
 
 chatbot = ChatBot('RIBA')
 trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train('chatterbot.corpus.english')
+trainer.train("chatterbot.corpus.english",
+"./rec/rec.yml"
+)
