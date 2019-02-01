904cf20a42423b54c2e30a9a2fe7d5a7bab1e2f8
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-02-01 13:02:57 +0530

    riba training

--- deploy/train_riba.py
+++ deploy/train_riba.py
@@ -1,21 +1,25 @@
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
 import os
+import dj_database_url
+
 print(os.getcwd())
 wd = os.getcwd()
-
-chatbot = ChatBot('RIBA')
-trainer = ChatterBotCorpusTrainer(chatbot)
 
 ON_HEROKU = os.environ.get('ON_HEROKU')
 
 if ON_HEROKU:
+    chatbot = ChatBot('RIBA',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri=dj_database_url.config())
+    trainer = ChatterBotCorpusTrainer(chatbot)
     os.system("wget http://riba-support.000webhostapp.com/RIBA/training-data/rec.yml")
     trainer.train("chatterbot.corpus.english",
     wd + "/rec.yml"
     )
 
 else: #on Travis
+    chatbot = ChatBot('RIBA')
     trainer.train("chatterbot.corpus.english",
     wd + "/rec/rec.yml"
     )
