ceec9a5e8e078ea57349941159e468417b8884f3
JAYAPRAVEEN <jayapraveen.ar.2015.cse@rajalakshmi.edu.in>
2019-02-01 19:45:44 +0530

    Update train_riba.py

--- deploy/train_riba.py
+++ deploy/train_riba.py
@@ -15,7 +15,7 @@ if ON_HEROKU:
     database_uri= uri)
     trainer = ChatterBotCorpusTrainer(chatbot)
     os.system("wget http://riba-support.000webhostapp.com/RIBA/training-data/rec.yml")
-    trainer.train("chatterbot.corpus.english.greetings",
+    trainer.train("chatterbot.corpus.english",
     wd + "/rec.yml"
     )
 
